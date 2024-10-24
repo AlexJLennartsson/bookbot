ab93af4f7e0fc6c23de262d0d1379e9f4c1ad9ac
AlexJLennartsson <alexlennartsson@gmail.com>
2024-10-24 19:56:09 +0100

    added function to generate dictionary of characters as well as filling that dictionary with the correct character count

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-book = open("books/frankenstein.txt", "r")
-print(book.read())
+def main():
+  book = open("books/frankenstein.txt", "r")
+  totalWords = 0
+  charDict = generateCharDict()
+  for line in book:
+    totalWords += len(line.split())
+    #for char in line:
+    for letter in line.lower():
+      if letter.isalpha():
+        charDict[letter] += 1
+  for letterCount in charDict:
+    print(f"The '{letterCount}' character was found {charDict[letterCount]} times")
+
+def generateCharDict():
+  charDict = {}
+  alphabet = list(map(chr, range(97, 123)))
+  for letter in alphabet:
+    charDict[letter] = 0
+  return charDict
+
+if __name__ == "__main__":
+  main()
